c659473140155680ffa3b8a09c21adc329c39c70
Mathew Martinez <mathew.martinez21@my.stjohns.edu>
2025-02-10 21:13:58 -0500

    finished homework

--- src/main/java/academy/javapro/Tesla.java
+++ src/main/java/academy/javapro/Tesla.java
@@ -4,10 +4,53 @@ public class Tesla extends Vehicle implements Electric, Autonomous {
     private boolean autopilotEnabled;
     private boolean charging;
 
-    // TODO: Create constructor that takes model and year
-    // TODO: Set make to "Tesla"
-    // TODO: Initialize autopilotEnabled and charging to false
+    public Tesla(String model, int year) {
+        super("Tesla", model, year);
+        this.autopilotEnabled = false;
+        this.charging = false;
+    }
+    
+    public void startEngine() {
+        this.isRunning = true;
+        System.out.println(this.make + " " + this.model + " " + "Engine started");
+    }
 
-    // TODO: Implement all required methods from Vehicle, Electric, and Autonomous
-    // Each method should include appropriate print statements
+    public void stopEngine() {
+        this.isRunning = false;
+        System.out.println(this.make + " " + this.model + " " + "Engine stopped");
+    }
+
+    public void accelerate() {
+        System.out.println(this.make + " " + this.model + " " + "is Accelerating");
+    }
+
+    public void brake() {
+        System.out.println(this.make + " " + this.model + " " + "is Braking");
+    }
+
+    public void enableAutopilot() {
+        this.autopilotEnabled = true;
+        System.out.println("Autopilot enabled");
+    }
+
+    public void disableAutopilot() {
+        this.autopilotEnabled = false;
+        System.out.println("Autopilot disabled");
+    }
+
+    public boolean isAutopilotEnabled() {
+           
+        return this.autopilotEnabled;
+    }
+
+    public void charge() {
+        this.charging = true;
+        System.out.println(this.make + " " + this.model + " " + "is charging");
+    }
+
+    public boolean isCharging() {
+        return this.charging;
+    }
+
+
 }
